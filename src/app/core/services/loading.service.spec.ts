d937cdd37c43e46cae4bb49c780510400eda8717
Milena Pavichevich <mila.pavichevich@gmail.com>
2026-06-22 22:24:42 -0300

    test(loading-service, loading-spinner): add unit tests for loading service and spinner

--- src/app/core/services/loading.service.spec.ts
+++ src/app/core/services/loading.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoadingService } from './loading.service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('LoadingService', () => {
   let service: LoadingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()]
+    });
     service = TestBed.inject(LoadingService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should start with loading false', () => {
+    expect(service.loading()).toBeFalse();
+  });
+
+  it('should set loading to true', () => {
+    service.show();
+    expect(service.loading()).toBeTrue();
+  })
+
+  it('should set loading to false', () => {
+    service.show();
+    service.hide();
+    expect(service.loading()).toBeFalse();
+  })
+
 });
--- src/app/shared/components/loading-spinner/loading-spinner.component.spec.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.spec.ts
@@ -1,17 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoadingSpinnerComponent } from './loading-spinner.component';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { LoadingService } from '../../../core/services/loading.service';
 
 describe('LoadingSpinnerComponent', () => {
   let component: LoadingSpinnerComponent;
   let fixture: ComponentFixture<LoadingSpinnerComponent>;
+  let loadingService: LoadingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoadingSpinnerComponent]
+      imports: [LoadingSpinnerComponent],
+      providers: [provideZonelessChangeDetection()]
     })
     .compileComponents();
-
+    loadingService = TestBed.inject(LoadingService);
     fixture = TestBed.createComponent(LoadingSpinnerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +24,20 @@ describe('LoadingSpinnerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not show spinner when loading is false', () => {
+    const overlay = fixture.nativeElement.querySelector('.overlay');
+
+    expect(overlay).toBeNull();
+  });
+
+  it('should show spinner when loading is true', () => {
+    loadingService.show();
+    fixture.detectChanges();
+
+    const overlay = fixture.nativeElement.querySelector('.overlay');
+    expect(overlay).toBeTruthy();
+  })
+
+
 });
