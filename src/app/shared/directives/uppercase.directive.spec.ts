6979278977ceae29c757462c58401cb88ee5d363
Milena Pavichevich <mila.pavichevich@gmail.com>
2026-06-22 22:25:44 -0300

    test(uppercase-directive): add unit tests for UppercaseDirective

--- src/app/shared/directives/uppercase.directive.spec.ts
+++ src/app/shared/directives/uppercase.directive.spec.ts
@@ -1,8 +1,52 @@
+import { Component, provideZonelessChangeDetection } from '@angular/core';
 import { UppercaseDirective } from './uppercase.directive';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+@Component({
+  imports: [ReactiveFormsModule, UppercaseDirective],
+  template: `
+    <input [formControl]="nameControl" appUppercase />
+  `,
+
+})
+class TestComponent {
+  nameControl = new FormControl('');
+}
+
 
 describe('UppercaseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UppercaseDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+  let input: HTMLInputElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestComponent],
+      providers: [provideZonelessChangeDetection()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    input = fixture.nativeElement.querySelector('input');
   });
+
+  it('should convert input value to uppercase', () => {
+    input.value = 'superman';
+    input.dispatchEvent(new Event('input'));
+
+    expect(component.nameControl.value).toBe('SUPERMAN');
+  });
+
+  it('should convert initial value to uppercase on init', () => {
+
+    const newFixture = TestBed.createComponent(TestComponent);
+    newFixture.componentInstance.nameControl.setValue('robin');
+    newFixture.detectChanges()
+
+    expect(newFixture.componentInstance.nameControl.value).toBe('ROBIN');
+  });
+
+
 });
